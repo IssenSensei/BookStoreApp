f06ef6a5d44903f816bf1c5dcc389db541df952a
Krystian Demidiuk <issensensei@gmail.com>
2021-02-15 20:15:10 +0100

    Display shelf book list

--- app/src/main/java/com/issen/ebooker/bookList/BookListFragment.kt
+++ app/src/main/java/com/issen/ebooker/bookList/BookListFragment.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
 import com.issen.ebooker.EBookerApplication
 import com.issen.ebooker.databinding.FragmentBookListBinding
 
 class BookListFragment : Fragment() {
 
     private lateinit var binding: FragmentBookListBinding
+    private val safeArgs: BookListFragmentArgs by navArgs()
 
     private val viewModel: BookListViewModel by viewModels {
         BookListViewModelFactory(
@@ -26,12 +28,17 @@ class BookListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentBookListBinding.inflate(inflater, container, false)
+        if (safeArgs.shelfId != -1) {
+            viewModel.getShelfBooks(safeArgs.shelfId)
+        }
 
         val adapter = BookListRecyclerViewAdapter()
         viewModel.bookList.observe(viewLifecycleOwner, Observer {
             adapter.submitList(it)
         })
 
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
         binding.bookListRecyclerView.adapter = adapter
 
         return binding.root
--- app/src/main/java/com/issen/ebooker/bookList/BookListViewModel.kt
+++ app/src/main/java/com/issen/ebooker/bookList/BookListViewModel.kt
@@ -1,6 +1,7 @@
 package com.issen.ebooker.bookList
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.issen.ebooker.data.BooksRepository
@@ -9,11 +10,22 @@ import kotlinx.coroutines.launch
 
 class BookListViewModel(private val booksRepository: BooksRepository) : ViewModel() {
 
-    val bookList: LiveData<List<Book>> = booksRepository.books
+    private val _bookList = MutableLiveData<List<Book>>()
+    val bookList: LiveData<List<Book>>
+        get() {
+           return _bookList
+        }
 
     init {
         viewModelScope.launch {
             booksRepository.refreshBooks()
         }
     }
+
+    fun getShelfBooks(id: Int) {
+        viewModelScope.launch {
+            _bookList.value = booksRepository.getShelfBooks(id)
+        }
+    }
+
 }
